d9492d0df832e50a8d30d95defeb479c917bcff5
Daniel Makiyama <daniel_desenho@hotmail.com>
2016-03-19 19:53:26 -0300

    Step 7 - Camera plugin usage

--- appSample/www/js/index.js
+++ appSample/www/js/index.js
@@ -35,6 +35,9 @@ var app = {
 var html = {
     li: function(txt){
         return "<li>" + txt + "</li>";
+    },
+    img: function(src, alt, cls, clsContainer){
+        return "<div class='" + clsContainer + "'><img alt='" + alt + "' src='" + src + "' class='" + cls + "' /></div>";
     }
 }
 
@@ -44,12 +47,31 @@ var communicator = (function(){
     //referencia a listview
     var messagesHtml = $('#messages');
 
-    var onSendMessage = function() {
-        var message = {text: $('#message').val()};
+    var storeMessage = function(type, val){
+        var message = {value: val, type: type};
         //inclui na lista de mensagens salvas
         savedMessages.push(message);
         //persiste na localStorage
         window.localStorage.setItem('messages', JSON.stringify(savedMessages));
+    }
+
+    var onGetPhoto = function(){
+        navigator.camera.getPicture(onGetPhotoSuccess, onGetPhotoError, 
+            //{ sourceType : Camera.PictureSourceType.PHOTOLIBRARY, destinationType : Camera.DestinationType.DATA_URL });
+            { sourceType : Camera.PictureSourceType.CAMERA, destinationType : Camera.DestinationType.DATA_URL });
+    }
+
+    var onGetPhotoSuccess = function(imageData){        
+        storeMessage('image64', imageData);
+        renderMessages(true);
+    }
+
+    var onGetPhotoError = function(err){
+        console.log('could not access photos ' + err);
+    }    
+
+    var onSendMessage = function() {
+        storeMessage('text', $('#message').val());
         //limpa o textarea
         $('#message').val('');
         //atualiza a lista no html
@@ -75,7 +97,11 @@ var communicator = (function(){
         //itera sobre a lista de mensagens salvas
         savedMessages.forEach(function(msg){
             //cria um li para cada item da lista de mensagens
-            messagesHtml.append(html.li(msg.text));
+            if(msg.type === 'text') {messagesHtml.append(html.li(msg.value));}
+            if(msg.type === 'image64'){
+                var src = "data:image/jpeg;base64," + msg.value;
+                messagesHtml.append(html.li(html.img(src, 'image','img-thumb', 'img-container')));
+            }
             //caso já exista uma listview criada, executa o comando de refresh
             // para aplicar o estilo novamente apos recriar a lista
             if(refresh) messagesHtml.listview('refresh');
@@ -92,7 +118,8 @@ var communicator = (function(){
             renderMessages();
             //registra os eventos de touch no botao de send-message e delete-messages
             $('#send-message').on("tap", onSendMessage);            
-            $('#delete-messages').on("tap", onDeleteMessages);
+            $('#delete-messages').on("tap", onDeleteMessages);            
+            $('#get-photo').on("taphold", onGetPhoto);            
         }
     };
 
